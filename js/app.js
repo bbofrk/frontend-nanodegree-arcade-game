9b851f45b8ae3b87e4c6edafb1536c3a4b591d3c
bbofrk <mingliwanglau@gmail.com>
2016-01-28 07:26:21 +1300

    draw map

--- js/app.js
+++ js/app.js
@@ -24,12 +24,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
 
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+
+};
+
+Player.prototype.handleInput = function(keyPressed) {
+
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+var allEnemies = [];
 // Place the player object in a variable called player
-
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
